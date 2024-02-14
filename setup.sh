46c321a718be399c1babab35573b8b94d69f8b99
Omar Mohamed <121226656+omarx69@users.noreply.github.com>
2024-02-14 15:58:09 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 sudo pacman -Syu; sudo pacman -S fish archcraft-i3wm git ttf-jetbrains-mono
 cd Pictures
 mkdir Wallpapers
-git clone https://github.com/Gingeh/wallpapers.git
+git clone https://github.com/Gingeh/wallpapers.git catpuchin_walls
+
